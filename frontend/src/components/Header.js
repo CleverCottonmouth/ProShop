e4128c5aa8f9c633cd1cc23403e48ded49c1a40d
mirajhad <mirajhadish48@gmail.com>
2024-07-06 16:04:40 +0530

    router added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import {LinkContainer} from 'react-router-bootstrap';
 import logo from '../assets/logoi.png';
 const Header= () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="/">
+      <LinkContainer to='/'>
+      <Navbar.Brand>
         <img src={logo} alt='logo' width={50}/>
         E-Commerce
         </Navbar.Brand>
+      </LinkContainer>     
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className="ms-auto">
-          <Nav.Link href="/cart"><FaShoppingCart/> Cart</Nav.Link>
-          <Nav.Link href="/login"><FaUser/> Login</Nav.Link>
+          <LinkContainer to='/cart'>
+          <Nav.Link><FaShoppingCart/> Cart</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/login'>
+          <Nav.Link><FaUser/> Login</Nav.Link>
+          </LinkContainer>
+          
           
           </Nav>
         </Navbar.Collapse>
