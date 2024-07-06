27934c741085892e7350d03af0818ac13947536e
mirajhad <mirajhadish48@gmail.com>
2024-07-06 12:37:25 +0530

    logo and bootstrap added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import logo from '../assets/logoi.png';
 const Header= () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="/">E-Commerce</Navbar.Brand>
+        <Navbar.Brand href="/">
+        <img src={logo} alt='logo' width={50}/>
+        E-Commerce
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className="ms-auto">
