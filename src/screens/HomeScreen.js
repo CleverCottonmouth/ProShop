574bdc18740ea364f727db1c0c96a55a62608537
mirajhad <mirajhadish48@gmail.com>
2024-07-08 13:33:28 +0530

    endpoints setup

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap';
-import Product from '../components/Product';
-import products from '../product';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 const HomeScreen = () => {
-   
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
   return (
     <>
-        <h1>Lastest Product</h1>
-        <Row>
-        {products.map((product)=>(
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product}/>
-            </Col>
-        ))}    
-        </Row>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Lastest Product</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
